6212d46507a4f11670e70ddc7a1da8516a97cbb3
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-12-23 23:25:41 +0530

    Update GovindStore.py

--- GovindStore.py
+++ GovindStore.py
@@ -3798,7 +3798,6 @@ else:
                     <div style="
                         background: linear-gradient(135deg, #1e293b, #0f172a);
                         color: #f8fafc;
-                        max-width: 240px; 
                         margin-bottom:14px;
                         padding: 16px;
                         border-radius: 18px;
