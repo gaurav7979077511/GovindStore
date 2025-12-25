52172285c73a01a6d65f306487ab5f38a02f4173
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-12-26 04:44:40 +0530

    Update GovindStore.py

--- GovindStore.py
+++ GovindStore.py
@@ -4525,10 +4525,10 @@ else:
                     ">
                         <div>
                             <div style="font-size:13px;font-weight:700;">
-                                {r['TransactionDate']}
+                                {pd.to_datetime(r['TransactionDate']).date()}
                             </div>
                             <div style="font-size:12px;color:#475569;">
-                                {pd.to_datetime(r['Category']).date()}
+                                {r['Category']}
                             </div>
                         </div>
 
