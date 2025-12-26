1c6cff53f7727294fbfb4200103cba850ce914b3
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-12-26 16:06:57 +0530

    Update GovindStore.py

--- GovindStore.py
+++ GovindStore.py
@@ -4651,7 +4651,7 @@ else:
                 """
 
 
-                components.html(row_html, height=100)
+                components.html(row_html, height=110)
 
             
     # ----------------------------
